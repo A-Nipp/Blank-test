31f061da561da4aa983b0a93a5570476bc7c70a7
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2022-10-12 16:32:51 -0400

    Initial Commit

---
+++ Blank-test/Blank_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blank_testApp.swift
+//  Blank-test
+//
+//  Created by AlecNipp on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Blank_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
